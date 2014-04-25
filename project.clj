fa2f0a0be57f51aa9f1d4b8ca806d401189392bb
thegeez <thegeez@users.noreply.github.com>
2014-04-25 10:55:22 +0200

    Make lein run work

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [enlive "1.0.0"]
                  [clj-time "0.3.7"]
-                 [ring "1.0.0"]])
+                 [ring "1.0.0"]]
+  :main clj-static-blog.dev-server/run-server)
